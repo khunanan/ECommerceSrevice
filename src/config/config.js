12444f82bc40ca78e0d18eb32fc33a89f11c7042
khunanan <khunanan.pan@gmail.com>
2022-06-06 22:37:28 +0700

    up project

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    host: "www.srichomthong.com",
+    host: "localhost",
     user: "master",
     password: "1qaz2wsx3edc",
     database: "ecommerce",
